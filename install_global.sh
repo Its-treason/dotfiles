f341db84fcf8bd13fb485aa47dcf25b28297daf2
Its-treason <39559178+Its-treason@users.noreply.github.com>
2023-06-15 23:17:34 +0200

    Add colors

--- install_global.sh
+++ install_global.sh
@@ -7,8 +7,9 @@ set -ex
 
 echo "Installing core utilities"
 apt update
+apt install -y --no-install-recommends curl htop git vim build-essential ca-certificates curl gnupg firefox-esr
 
-apt install -y --no-install-recommends curl htop git build-essential python3 python3-venv ca-certificates curl gnupg firefox-esr
+apt install -y --no-install-recommends i3 lightdm
 
 # Own repository
 echo "Adding packages.its-treason.com repo"
@@ -16,7 +17,7 @@ mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL http://packages.its-treason.com/ui/pgp | gpg --dearmor -o /etc/apt/keyrings/packages-its-treason.gpg
 echo \
 	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/packages-its-treason.gpg] \
-  https://packages.its-treason.com "${VERSION_CODENAME}" stable" |
+  https://packages.its-treason.com "${VERSION_CODENAME}" main" |
 	tee /etc/apt/sources.list.d/packages-its-treason.list >/dev/null
 
 apt update
@@ -30,17 +31,17 @@ echo \
 	tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # VsCode
 echo "Installing VsCode"
 curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/keyrings/microsoft-archive-keyring.gpg
 echo \
 	"deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] \
-  https://packages.microsoft.com/repos/vscode stable main" >/etc/apt/sources.list.d/vscode.list
+  https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list
 
 apt update
-apt install code
+apt install -y code
 
 # PHP
 case "$ID" in
@@ -49,7 +50,7 @@ case "$ID" in
 esac
 apt update
 
-for ver in "8.0", "8.1", "8.2", "7.0", "7.4"; do
+for ver in "8.0" "8.1" "8.2" "7.0" "7.4"; do
 	echo "Installing php-${ver}"
 	apt install php${ver}-amqp php${ver}-apcu php${ver}-bcmath php${ver}-bz2 php${ver}-cgi php${ver}-cli php${ver}-common php${ver}-curl php${ver}-decimal php${ver}-enchant php${ver}-excimer php${ver}-fpm php${ver}-gd php${ver}-gearman php${ver}-gmagick php${ver}-gmp php${ver}-gnupg php${ver}-grpc php${ver}-http php${ver}-imap php${ver}-inotify php${ver}-interbase php${ver}-intl php${ver}-libvirt-php php${ver}-lz4 php${ver}-mailparse php${ver}-maxminddb php${ver}-mbstring php${ver}-mcrypt php${ver}-memcache php${ver}-memcached php${ver}-mongodb php${ver}-msgpack php${ver}-mysql php${ver}-oauth php${ver}-odbc php${ver}-opcache php${ver}-pcov php${ver}-pgsql php${ver}-phpdbg php${ver}-pinba php${ver}-protobuf php${ver}-ps php${ver}-pspell php${ver}-psr php${ver}-raphf php${ver}-rdkafka php${ver}-readline php${ver}-redis php${ver}-rrd php${ver}-smbclient php${ver}-soap php${ver}-solr php${ver}-sqlite3 php${ver}-ssh2 php${ver}-stomp php${ver}-swoole php${ver}-sybase php${ver}-tideways php${ver}-tidy php${ver}-uopz php${ver}-uploadprogress php${ver}-uuid php${ver}-vips php${ver}-xdebug php${ver}-xhprof php${ver}-xml php${ver}-xmlrpc php${ver}-xsl php${ver}-yaml php${ver}-zip php${ver}-zmq php${ver}-zstd
 done
